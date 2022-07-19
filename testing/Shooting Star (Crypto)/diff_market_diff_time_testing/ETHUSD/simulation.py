6fe690f2f193c6aad22de5258763415fde7086b3
Daniel Galea <danielgalea@Daniels-MacBook-Pro.local>
2022-07-19 20:35:13 +0100

    Finished testing Shooting Star (Crypto) locally

--- testing/Shooting Star (Crypto)/diff_market_diff_time_testing/ETHUSD/simulation.py
+++ testing/Shooting Star (Crypto)/diff_market_diff_time_testing/ETHUSD/simulation.py
@@ -1,3 +1,4 @@
+from unittest import result
 from alpaca_trade_api.rest import REST, TimeFrame, TimeFrameUnit
 import pandas as pd
 import numpy as np
@@ -6,6 +7,7 @@ from datetime import timedelta as td
 import sys
 sys.path.insert(0, "/Users/danielgalea/Documents/git/stock_trading") 
 from common_funcs import load_credentials, process_quotes, log_trades
+import multiprocessing as mp
 
 def get_prices(api, symbol, start_date, end_date, period):
 
@@ -29,34 +31,37 @@ def get_prices(api, symbol, start_date, end_date, period):
 
 def get_quotes(api, symbol, init_date, offset=0):
     
-	if isinstance(init_date, np.datetime64):
-		init_date = pd.to_datetime(init_date).tz_localize("UTC").tz_convert(tz='America/New_York')
+    if isinstance(init_date, np.datetime64):
+        init_date = pd.to_datetime(init_date).tz_localize("UTC").tz_convert(tz='America/New_York')
 
-	if offset > 0:
-		init_date -= td(minutes=1)
-		
-	init_date_str = init_date.isoformat()
-	end_date_str = (init_date + td(minutes=1)).isoformat()
-    
-	quotes = api.get_crypto_quotes(symbol, init_date_str, end_date_str, limit=1000000).df
-    
-	while len(quotes) == 0:
-		init_date += td(minutes=1)
-		init_date_str = init_date.isoformat()
-		end_date_str = (init_date + td(minutes=1)).isoformat()
-		quotes = api.get_crypto_quotes(symbol, init_date_str, end_date_str, limit=1000000).df
+    if offset > 0:
+        init_date -= td(minutes=1)
+        
+    init_date_str = init_date.isoformat()
+    end_date_str = (init_date + td(minutes=1)).isoformat()
 
-	quotes = process_quotes(quotes)
-    
-	return init_date, quotes
+    quotes = api.get_crypto_quotes(symbol, init_date_str, end_date_str, limit=1000000).df
+
+    while len(quotes) == 0:
+        init_date += td(minutes=1)
+        init_date_str = init_date.isoformat()
+        end_date_str = (init_date + td(minutes=1)).isoformat()
+        quotes = api.get_crypto_quotes(symbol, init_date_str, end_date_str, limit=1000000).df
+
+    quotes = process_quotes(quotes)
+
+    return init_date, quotes
  
 def simulation(prices, shadow_length):
 
-    open_trades = []
-    close_trades = []
+    open_trades_times = []
+    close_trades_times = []
 
-    last_close_trade = None
-
+    years = prices.index.year.values
+    months = prices.index.month.values
+    days = prices.index.day.values
+    hours = prices.index.hour.values
+    
     for row in range(len(prices)-1):
         
         index = prices.index.values[row]
@@ -66,15 +71,9 @@ def simulation(prices, shadow_length):
         high_price = values.high
         low_price = values.low
 
-        if last_close_trade != None and pd.to_datetime(index).tz_localize("UTC").tz_convert(tz='America/New_York') <= last_close_trade:
+        if years[row] == 2018 and months[row] == 2 and (days[row] in [3, 4, 5] or (days[row] == 2 and hours[row] >= 17)):
             continue
 
-        if prices.index.year.values[row] == 2018 and prices.index.month.values[row] == 2 and (prices.index.day.values[row] in [2, 3, 4, 5] or (prices.index.day.values[row] == 2 and prices.index.hour.values[row] >= 17)):
-            continue
-
-        if prices.index.hour.values[row] == 0 and prices.index.minute.values[row] == 0:
-            print(index)
-        
         body_length = abs(open_price - close_price)
         if open_price <= close_price:
             upper_wick_length = abs(high_price - close_price)
@@ -84,36 +83,76 @@ def simulation(prices, shadow_length):
             lower_wick_length = abs(close_price - low_price)
 
         if close_price > open_price and lower_wick_length > upper_wick_length and lower_wick_length * shadow_length > body_length:
-            if len(open_trades) == len(close_trades):
-                date, quotes = get_quotes(api, symbol, index, offset=0)
-                open_trades.append([date, quotes.median_ask_price[0]])
+            if len(open_trades_times) == len(close_trades_times):
+                open_trades_times.append(index)
                 
         if close_price < open_price and upper_wick_length > lower_wick_length and upper_wick_length * shadow_length > body_length:
-            if len(open_trades) - 1 == len(close_trades):
-                date, quotes = get_quotes(api, symbol, index, offset=0)
-                close_trades.append([date, quotes.median_bid_price[0]])
-                last_close_trade = date
+            if len(open_trades_times) - 1 == len(close_trades_times):
+                close_trades_times.append(index)
         
-    if len(open_trades) - 1 == len(close_trades):
-        open_trades = open_trades[:-1]
+    inter_open_trades = []
+    inter_close_trades = []
+
+    inputs = []
+    for open_time in open_trades_times:
+        inputs.append([api, symbol, open_time])
+
+    pool = mp.Pool(mp.cpu_count() * 2)
+    results = pool.starmap(get_quotes, inputs, chunksize=1)
+
+    for res in results:
+        date, quotes = res
+        inter_open_trades.append([date, quotes.median_ask_price[0]])
         
+    inputs = []
+    for close_time in close_trades_times:
+        inputs.append([api, symbol, close_time])
+
+    results = pool.starmap(get_quotes, inputs, chunksize=1)
+
+    for res in results:
+        date, quotes = res
+        inter_close_trades.append([date, quotes.median_bid_price[0]])
+
+    if len(inter_open_trades) - 1 == len(inter_close_trades):
+        inter_open_trades = inter_open_trades[:-1]
+
+    pool.close()
+
+    open_trades = []
+    close_trades = []
+
+    open_counter = 1
+    close_counter = 0
+
+    open_trades.append(inter_open_trades[0])
+    while open_counter < len(inter_open_trades):
+        if inter_close_trades[close_counter][0] >= inter_open_trades[open_counter][0]:
+            pass
+        else:
+            close_trades.append(inter_close_trades[close_counter])
+            open_trades.append(inter_open_trades[open_counter])
+        open_counter += 1
+        close_counter += 1
+    close_trades.append(inter_close_trades[close_counter])
+    
     return open_trades, close_trades
         
 if __name__ == "__main__":
 
-	symbol = sys.argv[1]
-	start_date = "2017-07-01"
-	end_date = "2018-07-01"
-	period = float(sys.argv[2])
-	shadow_length = 1
+    symbol = sys.argv[1]
+    start_date = "2017-07-01"
+    end_date = "2018-07-01"
+    period = int(sys.argv[2])
+    shadow_length = 1
 
-	key_id, secret_key = load_credentials(opt="paper")
-	api = REST(key_id, secret_key, base_url='https://paper-api.alpaca.markets')
-	
-	print("Getting prices...")
-	prices = get_prices(api, symbol, start_date, end_date, period)
-	print("Prices pbtained.")
+    key_id, secret_key = load_credentials(opt="paper")
+    api = REST(key_id, secret_key, base_url='https://paper-api.alpaca.markets')
+    
+    print("Getting prices...")
+    prices = get_prices(api, symbol, start_date, end_date, period)
+    print("Prices pbtained.")
 
-	open_trades, close_trades = simulation(prices, shadow_length)
-
-	log_trades(open_trades, close_trades, symbol, sys.argv[2])
+    open_trades, close_trades = simulation(prices, shadow_length)
+    
+    log_trades(open_trades, close_trades, symbol, sys.argv[2])
